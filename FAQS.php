59a1acca9580e8c5d3ce90d848e04903b1513ed2
walczak <adrien.walczak@le-campus-numerique.fr>
2023-01-03 13:31:39 +0100

    coursphp

---
+++ FAQS.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+
+
+<head>
+
+
+</head>
+
+<body>
+
+<?php include ('header.php'); ?>
+
+<h1>F</h1>
+
+<?php include ('footer.php'); ?>
+
+</body>
+
+</html>
---
+++ pagePrincipale.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="fr">
+
+
+<head>
+    <meta charset="utf-8">
+    <title>Page Principale</title>
+</head>
+
+<body>
+
+<?php include ('header.php'); ?>
+
+<h1>Bienvenue sur le site</h1>
+
+<h3>Un petit lorem ipsum</h3>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum non consectetur a erat nam at lectus urna. Orci a scelerisque purus semper. Eu feugiat pretium nibh ipsum consequat nisl. At in tellus integer feugiat scelerisque varius. Ut faucibus pulvinar elementum integer enim neque volutpat. Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Commodo nulla facilisi nullam vehicula ipsum a. Diam in arcu cursus euismod quis viverra nibh cras. Tincidunt tortor aliquam nulla facilisi.
+
+    Sed vulputate mi sit amet. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Purus viverra accumsan in nisl nisi scelerisque eu. Sit amet commodo nulla facilisi nullam vehicula. Turpis massa tincidunt dui ut. Semper auctor neque vitae tempus quam pellentesque nec. Quam id leo in vitae. Consectetur adipiscing elit pellentesque habitant. Morbi tincidunt augue interdum velit. Dui accumsan sit amet nulla.
+
+    Sit amet venenatis urna cursus eget nunc scelerisque viverra. Viverra aliquet eget sit amet. Tempus quam pellentesque nec nam. Pellentesque dignissim enim sit amet venenatis urna. Sem nulla pharetra diam sit. Pulvinar mattis nunc sed blandit libero. Adipiscing diam donec adipiscing tristique risus nec feugiat in fermentum. Nunc congue nisi vitae suscipit. Nec nam aliquam sem et. Tristique senectus et netus et malesuada fames ac turpis egestas. Nibh ipsum consequat nisl vel. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Eget sit amet tellus cras adipiscing enim eu turpis. Accumsan sit amet nulla facilisi. Eget mauris pharetra et ultrices neque ornare aenean euismod. Quis risus sed vulputate odio ut enim. Dui id ornare arcu odio ut sem. At in tellus integer feugiat scelerisque varius morbi enim nunc. Proin sagittis nisl rhoncus mattis. A erat nam at lectus urna.
+
+    Cras fermentum odio eu feugiat pretium nibh ipsum consequat. Consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Egestas egestas fringilla phasellus faucibus scelerisque. Urna neque viverra justo nec. Quis commodo odio aenean sed adipiscing. Lectus magna fringilla urna porttitor rhoncus. Viverra nam libero justo laoreet sit amet. Faucibus vitae aliquet nec ullamcorper. Vulputate ut pharetra sit amet. Amet purus gravida quis blandit turpis cursus. Vitae et leo duis ut diam. Sit amet aliquam id diam. Egestas purus viverra accumsan in nisl nisi scelerisque eu. Proin nibh nisl condimentum id. Tortor posuere ac ut consequat semper viverra nam libero. Consectetur adipiscing elit duis tristique sollicitudin nibh sit. Purus in massa tempor nec feugiat nisl pretium fusce id. Felis eget nunc lobortis mattis aliquam faucibus purus in. Velit aliquet sagittis id consectetur purus. Sit amet massa vitae tortor condimentum lacinia quis vel.
+
+    Vitae auctor eu augue ut lectus. Viverra accumsan in nisl nisi scelerisque eu ultrices. Ac odio tempor orci dapibus ultrices in iaculis nunc. Hendrerit gravida rutrum quisque non tellus orci ac. Egestas purus viverra accumsan in nisl nisi scelerisque. Leo urna molestie at elementum eu facilisis sed odio morbi. Leo a diam sollicitudin tempor id eu nisl nunc mi. Laoreet suspendisse interdum consectetur libero. In egestas erat imperdiet sed euismod nisi porta lorem. Euismod quis viverra nibh cras pulvinar. Egestas maecenas pharetra convallis posuere morbi leo urna molestie.</p>
+
+<?php include ('footer.php'); ?>
+
+</body>
+
+</html>
