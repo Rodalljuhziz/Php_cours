72adda272856179a4d3ccd4c7312b596a5bd75a1
walczak <adrien.walczak@le-campus-numerique.fr>
2023-01-04 10:00:27 +0100

    testformulaire

--- Contact.php
+++ Contact.php
@@ -4,12 +4,13 @@
 
 <head>
     <meta charset="utf-8">
+    <link href="css/headers.css" rel="stylesheet">
     <title>Contact</title>
 </head>
 
 <body>
 
-<h1>Entrez vos coordonnées</h1>
+<h1>Entrez vos coordonnées:</h1>
 
 <form action="" method="post">
     <div>
@@ -21,21 +22,41 @@
     </div>
     <div>
         <label for="name">Nom: </label>
-        <input type="text" id="name" name="user_name">
-    </div>
-    <div>
+        <input type="text" id="name" name="user_name" >
+
         <label for="firstname">Prénom: </label>
         <input type="text" id="firstname" name="user_firstname">
-    </div>
-    <div>
+
         <label for="mail">Mail: </label>
         <input type="email" id="mail" name="user_mail">
     </div>
     <div>
-        <label for="reason">Object de la demande: </label>
-        <input type="radio" id="reason" name="user_reason">
-    </div>
+        <input type="radio" id="reasonJob" name="user_reason">
+        <label for="reason">Proposition d'emploi </label><br>
 
+        <input type="radio" id="reasonInfo" name="user_reason">
+        <label for="reason">demande d'information </label><br>
+
+        <input type="radio" id="reasonPrest" name="user_reason">
+        <label for="reason">prestation </label>
+
+    </div>
+    <div>
+        <label for="mesage">Message: </label>
+        <textarea rows="4" id="message" name="user_message"></textarea>
+    </div>
+    <div>
+        <input type="submit" value="Valider">
+    </div>
+    <?php
+        filter_input(
+                $name,
+                $firstName,
+                $mail = FILTER_VALIDATE_EMAIL,
+                $reason
+
+        )
+    ?>
 </form>
 
 </body>
